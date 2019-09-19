e766a9182f912e8e32605cf2d4e08e1bdad101ef
Evgeniy Tarasov <tarasov@ucontent.ru>
2019-09-19 12:35:40 +0300

    las commit

--- TarasovEA_HW2.1.1/ViewController.swift
+++ TarasovEA_HW2.1.1/ViewController.swift
@@ -15,10 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func myButton() {
-        
-        
-    }
+    
     
 }
 
